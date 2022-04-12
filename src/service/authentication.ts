8183207fab69ee5cf3605a27a4ad47e13824cbba
myesn <myesn@foxmail.com>
2022-04-12 20:45:00 +0800

    :label: 新增 AuthenticationClient

---
+++ src/service/authentication.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosInstance } from "axios";
+
+export default class AuthenticationClient {
+  private http: AxiosInstance;
+  constructor() {
+    this.http = axios.create({
+      baseURL: process.env.AUTHENTICATION_URL,
+    });
+  }
+
+  async findByToken(token: string) {
+    const response = await this.http.get<AuthenticationFindByTokenResult>(
+      "/find-by-token"
+    );
+    return response.data;
+  }
+}
+
+export type AuthenticationFindByTokenResult = {
+  /** 用户 ID */
+  id: string;
+};
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,1 +1,2 @@
 export { default as TestService } from "./test";
+export { default as AuthenticationClient } from "./authentication";
