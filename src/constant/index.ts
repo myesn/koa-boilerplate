2450f48f398d9988512943e3f3504120de56b223
myesn <myesn@foxmail.com>
2022-04-18 22:21:50 +0800

    :bug: fix __dirname not exists

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,9 @@
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+// https://docs.joshuatz.com/cheatsheets/node-and-npm/node-esm/#nodejs-built-ins-and-global-objects
+export const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const assetDirectory = path.join(__dirname, "..", "/assets");
 export const tempDirectory = path.join(assetDirectory, "temp");
 
