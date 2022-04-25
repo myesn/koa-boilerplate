57875d85257186ae7a0b2a9a45a5adde32d054e6
myesn <myesn@foxmail.com>
2022-04-25 17:00:09 +0800

    :sparkles: datetimeUtils 新增一些函数

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,9 +1,13 @@
-import dayjs from "dayjs";
+import dayjs, { ManipulateType, OpUnitType, QUnitType } from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import duration from "dayjs/plugin/duration";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(duration);
+dayjs.extend(customParseFormat);
 dayjs.tz.setDefault("Asia/Shanghai");
 
 // console.log(dayjs.tz.guess());
@@ -24,8 +28,40 @@ function format(date: Date, format: string) {
   return dayjs(date).format(format);
 }
 
-function isSameDay(date1: Date, date2:Date){
+function isSameDay(date1: Date, date2: Date) {
   return dayjs(date1).isSame(dayjs(date2), "day");
 }
 
-export default { getUTCDateTimeNow, getFormattedNow, getNow, format, isSameDay };
+function add(date: Date, value: number, unit?: ManipulateType) {
+  return dayjs(date).add(value, unit);
+}
+
+function offsetDay(date: Date, value: number) {
+  return add(date, value, "day");
+}
+
+function beginOfDay(date: Date) {
+  return dayjs(date).startOf("day");
+}
+
+/** 返回指定单位下两个日期时间之间的差异 https://day.js.org/docs/zh-CN/display/difference */
+function diff(date1: Date, date2: Date, unit?: QUnitType | OpUnitType) {
+  return dayjs(date1).diff(date2, unit);
+}
+
+function toDayjs(date: dayjs.ConfigType, format?: dayjs.OptionType) {
+  return dayjs(date, format);
+}
+
+export default {
+  getUTCDateTimeNow,
+  getFormattedNow,
+  getNow,
+  format,
+  isSameDay,
+  add,
+  offsetDay,
+  beginOfDay,
+  diff,
+  toDayjs,
+};
