b907eba1344de5f01bd9d61c90a589fcd1f14187
myesn <myesn@foxmail.com>
2022-04-26 16:30:18 +0800

    :art: 细分 CommonSchema 规则

--- src/schema/common.ts
+++ src/schema/common.ts
@@ -1,7 +1,11 @@
 import Joi from "joi";
 
+export const skip = Joi.number().integer().min(0).required();
+export const limit = Joi.number().integer().positive().required();
+export const keyword = Joi.allow(null, "", Joi.string(), Joi.number());
+
 export const paging = {
-  skip: Joi.number().integer().min(0).required(),
-  limit: Joi.number().integer().positive().required(),
-  keyword: Joi.allow(null, "", Joi.string(), Joi.number()),
+  skip,
+  limit,
+  keyword,
 };
