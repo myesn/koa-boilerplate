56a3e2f47c292fd55b7bf1c4ca3b97596b44e958
myesn <myesn@foxmail.com>
2022-05-07 15:24:02 +0800

    :art: queryTypes 中间件中根据规则跳过处理

--- src/middleware/queryTypes.ts
+++ src/middleware/queryTypes.ts
@@ -5,6 +5,13 @@ import { KoaCustomAppContext } from "../project";
 // https://github.com/ljharb/qs#parsing-primitivescalar-values-numbers-booleans-null-etc
 // koa 的 ctx.request.query 对象中字段的值只会是 string 类型，通过以下方式转换为实际类型
 export default () => async (ctx: KoaCustomAppContext, next: Next) => {
+  // 有些时候会把字符串转为数字，比如 qrCode=001，得到的结果为 qrCode=1，所以对于一些路径需要跳过此中间件的处理
+  const ignorePaths = ["/api/open/mobile-care/patient/detail-simple"];
+  if (ignorePaths.some((x) => ctx.request.path.startsWith(x))) {
+    await next();
+    return;
+  }
+
   // @ts-ignore
   const qq = qs.parse(ctx.request.querystring, {
     decoder(str, decoder, charset) {
